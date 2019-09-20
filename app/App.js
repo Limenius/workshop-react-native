e03cfe8bb2e0755bf03712f0327bd13e7c444ccd
Victoria Quirante <victoria@limenius.com>
2019-09-20 23:19:12 +0200

    Navigation playground

--- app/App.js
+++ app/App.js
@@ -1,14 +1,35 @@
-import React, { Component } from "react";
+import React, {Component} from 'react';
 
-import Movie from "./components/Movie";
-import MovieList from "./components/MovieList";
+import Movie from './components/Movie';
+import MovieList from './components/MovieList';
+import Actor from './components/Actor';
 
-export default class App extends Component {
-  render() {
-    return <Movie />;
-  }
+import {StyleSheet} from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import Colors from './styles/colors';
 
-  //render() {
-  //  return <MovieList/>
-  //}
-}
+const Styles = StyleSheet.create({
+  header: {
+    backgroundColor: Colors.subtleAccent,
+  },
+});
+
+const AppNavigator = createStackNavigator(
+  {
+    Home: {screen: MovieList},
+    Movie: {screen: Movie},
+
+    // Add the Actor screen
+  },
+  {
+    navigationOptions: {
+      headerTintColor: Colors.accent,
+      headerStyle: Styles.header,
+    },
+  },
+);
+
+const App = createAppContainer(AppNavigator);
+
+export default App;
